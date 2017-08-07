9093d7ad493f62ebfca127e0081bfec4936ea857
luixlacrux <luixlacrux18@gmail.com>
2017-08-07 18:43:06 -0400

    ADD: webpack config and basic server

---
+++ source/server.js
@@ -0,0 +1,32 @@
+import http from 'http';
+import React from 'react';
+import { renderToString } from 'react-dom/server';
+import { StaticRouter } from 'react-router';
+import Pages from './pages/containers/Page.jsx';
+
+function requestHandler(request, response) {
+  const context = {};
+
+  const html = renderToString(
+    <StaticRouter location={request.url} context={context}>
+      <Pages />
+    </StaticRouter>
+  );
+
+  response.setHeader('Content-Type', 'text/html');
+
+  if (context.url) {
+    response.writeHead(301, {
+      Location: context.url,
+    });
+
+    response.end();
+  }
+
+  response.write(html);
+  response.end();
+}
+
+const server = http.createServer(requestHandler);
+
+server.listen(3000);
---
+++ webpack/webpack.server.config.js
@@ -0,0 +1,28 @@
+const path = require('path')
+
+module.exports = {
+  entry: './source/server.js',
+  output: {
+    filename: 'index.js',
+    path: path.resolve(__dirname, '../built/server'),
+  },
+  module: {
+    rules: [
+      {
+        test: /\.json$/,
+        loader: 'json-loader',
+      },
+      {
+        test: /\.jsx?$/,
+        exclude: /(node_modules)/,
+        use: {
+          loader: 'babel-loader',
+          options: {
+            presets: ['latest-minimal', 'react']
+          },          
+        },
+      },
+    ]
+  },
+  target: 'node',
+}
