00366a66918acac8ad1227a762832b50bfae80f3
luixlacrux <luixlacrux18@gmail.com>
2017-08-07 19:44:00 -0400

    ADD: webpack client config

---
+++ webpack/webpack.client.config.js
@@ -0,0 +1,29 @@
+const path = require('path')
+
+module.exports = {
+  entry: './source/client.js',
+  output: {
+    filename: 'app.js',
+    path: path.resolve(__dirname, '../built/static'),
+  },
+  module: {
+    rules: [
+      {
+        test: /\.json$/,
+        loader: 'json-loader',
+      },
+      {
+        test: /\.jsx?$/,
+        exclude: /(node_modules)/,
+        use: {
+          loader: 'babel-loader',
+          options: {
+            presets: ['es2016', 'es2017', 'react'],
+            plugins: ['transform-es2015-modules-commonjs'],
+          },          
+        },
+      },
+    ]
+  },
+  target: 'node',
+}
